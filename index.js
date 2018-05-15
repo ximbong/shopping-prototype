62549153b846544857679467de88d994860f7238
Hai Truong <ximbong91023@users.noreply.github.com>
2018-05-15 13:59:54 +0300

    Basic functionality and pop-up added.

--- index.js
+++ index.js
@@ -1,24 +1,122 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-  const wishArray = [];
   const favoriteArray = [];
-  const toDo = {};
+  let toBuy = [];
+  const wrapper = document.querySelector('.wrapper');
+  const form = document.querySelectorAll('.form');
+  const body = document.querySelector('body');
+
+  const displayPopup = (id) => {
+    document.querySelector(`#${id} .checkboxes`).innerHTML = '';
+    document.getElementById(id).style.display = 'flex';
+    wrapper.style.display = 'flex';
+    body.style.overflow = 'hidden';
+  }
+
+  const hidePopup = (element) => {
+    element.style.display = 'none';
+    wrapper.style.display = 'none';
+    body.style.overflow = 'auto';
+  }
 
   document.addEventListener("click", function(e) {
     if (e.target.classList.contains('action')) {
       const action = e.target.classList[2];
+      e.target.classList.toggle('effect');
       const name = e.target.parentElement.parentElement.previousElementSibling.children[1].children[0].textContent;
       if (action === 'like') {
-        favoriteArray.push(name);
-      } else if (action === 'love') {
-        wishArray.push(name)
+        if (favoriteArray.indexOf(name) === -1) {
+          favoriteArray.push(name);
+          console.log(favoriteArray)
+        } else {
+          favoriteArray.splice(favoriteArray.indexOf(name), 1);
+          console.log(favoriteArray)
+        }
       }
-      console.log(action, name)
+      if (action === 'add') {
+        let removeIndex = -1;
+        toBuy.forEach(function(item, index) {
+          if ((item.name) === name) {
+            removeIndex = index;
+          }
+        })
+        if (removeIndex === -1) {
+          toBuy.push({
+            name: name,
+            quantity: 1
+          })
+        } else {
+          toBuy.splice(removeIndex, 1)
+        }
+      }
+
     }
-    if(e.target.classList.contains('button')){
+    if (e.target.classList.contains('button')) {
       const section = e.target.textContent;
-      if (section === "Wish list") {
-        console.log('Open wish list')
+      if (section === "Favorite") {
+        displayPopup('favorite-form');
+
+        for (let element of favoriteArray) {
+          const label = document.createElement("label");
+          const input = document.createElement("input");
+          const span = document.createElement("span");
+          const textNode = document.createTextNode(element);
+          label.classList.add('line');
+          label.setAttribute("for", element)
+          input.setAttribute("type", "checkbox");
+          input.id = element;
+          span.classList.add("checkmark");
+          label.appendChild(textNode);
+          label.appendChild(input);
+          label.appendChild(span);
+          document.querySelector("#favorite-form .checkboxes").appendChild(label);
+        }
+      }
+      if (section === "To-buy list") {
+        displayPopup('to-buy-form');
+
+        for (let element of toBuy) {
+          document.querySelector(`#to-buy-form .checkboxes`).innerHTML +=
+            `
+          <div class="to-buy-list">
+            <div class="tb-item">
+              <div class="tb-details">
+                <div class="tb-name">${element.name}</div>
+                <input type="number" value="${element.quantity}">
+              </div>
+              <i class="fas fa-times delete-item"></i>
+            </div>
+          </div>
+          `
+        }
       }
     }
   })
+
+
+  for (let element of form) {
+    element.addEventListener("click", function(e) {
+      if (e.target.classList.contains('form-close')) {
+        hidePopup(element);
+      }
+      if (e.target.classList.contains('delete-item')) {
+        const thisItem = e.target.parentElement;
+        thisItem.remove();
+        console.log(thisItem)
+      }
+      if (e.target.classList.contains('update-tb')) {
+        hidePopup(element);
+        let newArray = [].slice.call(document.querySelectorAll("#to-buy-form .tb-details"));
+        newArray = newArray.map(function(item) {
+          let name = item.querySelector(".tb-name").textContent;
+          let number = item.querySelector("input").value;
+          return {
+            name: name,
+            quantity: number
+          };
+        })
+        toBuy = newArray;
+        console.log(toBuy);
+      }
+    })
+  }
 });
